54a5a25186021e8c9e0b464baef1b6fbd6a5f71e
Stoyan Zhelyazkov <stoyan.zhelyazkov@broadcom.com>
2025-07-28 17:38:46 +0300

    create config profile resource
    Signed-off-by: Stoyan Zhelyazkov <stoyan.zhelyazkov@broadcom.com>

---
+++ vsphere/resource_vsphere_config_profile.go
@@ -0,0 +1,139 @@
+// © Broadcom. All Rights Reserved.
+// The term "Broadcom" refers to Broadcom Inc. and/or its subsidiaries.
+// SPDX-License-Identifier: MPL-2.0
+
+package vsphere
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/vmware/govmomi/vapi/cis/tasks"
+	"github.com/vmware/govmomi/vapi/esx/settings/clusters/configuration"
+	"github.com/vmware/govmomi/vapi/esx/settings/clusters/enablement"
+)
+
+func resourceVSphereConfigProfile() *schema.Resource {
+	return &schema.Resource{
+		CreateContext: resourceVSphereConfigProfileCreate,
+		ReadContext:   resourceVSphereConfigProfileRead,
+		UpdateContext: resourceVSphereConfigProfileUpdate,
+		DeleteContext: resourceVSphereConfigProfileDelete,
+		Schema: map[string]*schema.Schema{
+			"reference_host_id": {
+				Type:          schema.TypeString,
+				Optional:      true,
+				ConflictsWith: []string{"config"},
+				Description:   "The identifier of the host to use as a source of the configuration.",
+			},
+			"config": {
+				Type:          schema.TypeString,
+				Optional:      true,
+				Computed:      true,
+				ConflictsWith: []string{"reference_host_id"},
+				Description:   "The source configuration.",
+			},
+			"schema": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "The configuration schema.",
+			},
+			"cluster_id": {
+				Type:        schema.TypeString,
+				Required:    true,
+				Description: "The identifier of the cluster that will be configured.",
+			},
+		},
+	}
+}
+
+func resourceVSphereConfigProfileCreate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	referenceHostId := d.Get("reference_host_id").(string)
+	config := d.Get("config").(string)
+
+	if referenceHostId != "" && config != "" {
+		return diag.FromErr(fmt.Errorf("cannot specify both `reference_host_id` and `config`"))
+	}
+
+	clusterId := d.Get("cluster_id").(string)
+
+	client := meta.(*Client).restClient
+	m := enablement.NewManager(client)
+
+	if taskId, err := m.CheckEligibility(clusterId); err != nil {
+		return diag.FromErr(fmt.Errorf("failed to run eligibility check: %s", err))
+	} else if _, err := tasks.NewManager(client).WaitForCompletion(ctx, taskId); err != nil {
+		return diag.FromErr(err)
+	}
+
+	if referenceHostId != "" {
+		if taskId, err := m.ImportFromReferenceHost(clusterId, referenceHostId); err != nil {
+			return diag.FromErr(fmt.Errorf("failed to import configuration from reference host: %s", err))
+		} else if _, err := tasks.NewManager(client).WaitForCompletion(ctx, taskId); err != nil {
+			return diag.FromErr(err)
+		}
+	} else {
+		spec := enablement.FileSpec{Config: config}
+		if importResult, err := m.ImportFromFile(clusterId, spec); err != nil {
+			return diag.FromErr(fmt.Errorf("failed to import configuration: %s", err))
+		} else {
+			fmt.Println(importResult.Status)
+		}
+	}
+
+	if taskId, err := m.ValidateConfiguration(clusterId); err != nil {
+		return diag.FromErr(fmt.Errorf("failed to validate configuration: %s", err))
+	} else if _, err := tasks.NewManager(client).WaitForCompletion(ctx, taskId); err != nil {
+		return diag.FromErr(err)
+	}
+
+	if taskId, err := m.RunPrecheck(clusterId); err != nil {
+		return diag.FromErr(fmt.Errorf("failed to run precheck: %s", err))
+	} else if _, err := tasks.NewManager(client).WaitForCompletion(ctx, taskId); err != nil {
+		return diag.FromErr(err)
+	}
+
+	if taskId, err := m.EnableClusterConfiguration(clusterId); err != nil {
+		return diag.FromErr(fmt.Errorf("failed to enable cluster configuration: %s", err))
+	} else if _, err := tasks.NewManager(client).WaitForCompletion(ctx, taskId); err != nil {
+		return diag.FromErr(err)
+	}
+
+	return resourceVSphereConfigProfileRead(ctx, d, meta)
+}
+
+func resourceVSphereConfigProfileRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	client := meta.(*Client).restClient
+	m := configuration.NewManager(client)
+
+	clusterId := d.Get("cluster_id").(string)
+
+	d.SetId(fmt.Sprintf("config_profile_%s", clusterId))
+
+	config, err := m.GetConfiguration(clusterId)
+	if err != nil {
+		return diag.FromErr(fmt.Errorf("failed to retrieve cluster configuration: %s", err))
+	}
+
+	_ = d.Set("config", config.Config)
+
+	configSchema, err := m.GetSchema(clusterId)
+	if err != nil {
+		return diag.FromErr(fmt.Errorf("failed to retrieve configuration schema: %s", err))
+	}
+
+	_ = d.Set("schema", configSchema.Schema)
+
+	return nil
+}
+
+func resourceVSphereConfigProfileUpdate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	return diag.FromErr(fmt.Errorf("not implemented"))
+}
+
+func resourceVSphereConfigProfileDelete(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	// Can't go back after management via config profiles is enabled
+	return nil
+}
---
+++ vsphere/resource_vsphere_config_profile_test.go
@@ -0,0 +1,47 @@
+// © Broadcom. All Rights Reserved.
+// The term "Broadcom" refers to Broadcom Inc. and/or its subsidiaries.
+// SPDX-License-Identifier: MPL-2.0
+
+package vsphere
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+)
+
+func TestAccResourceVSphereConfigProfile(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			RunSweepers()
+			testAccPreCheck(t)
+		},
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccResourceVSphereConfigProfileConfig(),
+				Check:  resource.ComposeTestCheckFunc(),
+			},
+		},
+	})
+}
+
+func testAccResourceVSphereConfigProfileConfig() string {
+	return fmt.Sprintf(`
+%s
+
+resource "vsphere_config_profile" "profile" {
+  #reference_host_id = data.vsphere_host.roothost2.id
+  #reference_host_id = "host-10"
+  #cluster_id = data.vsphere_compute_cluster.rootcompute_cluster1.id
+  cluster_id = "domain-c49"
+  config = file("~/git/terraform-provider-vsphere/config.json")
+}
+`,
+		"")
+	//testhelper.CombineConfigs(
+	//	testhelper.ConfigDataRootDC1(),
+	//	testhelper.ConfigDataRootComputeCluster1(),
+	//	testhelper.ConfigDataRootHost2()))
+}

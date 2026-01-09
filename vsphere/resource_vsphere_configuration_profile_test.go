c2d2c1ca7ad51bdad82a66117a2a921c162a7783
Stoyan Zhelyazkov <stoyan.zhelyazkov@broadcom.com>
2026-01-09 17:06:07 +0200

    fix: configuration profiles update operation
    Signed-off-by: Stoyan Zhelyazkov <stoyan.zhelyazkov@broadcom.com>

--- vsphere/resource_vsphere_configuration_profile_test.go
+++ vsphere/resource_vsphere_configuration_profile_test.go
@@ -36,6 +36,27 @@ func TestAccResourceVSphereConfigProfile(t *testing.T) {
 	})
 }
 
+func TestAccResourceVSphereConfigProfile_debug(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			RunSweepers()
+			testAccPreCheck(t)
+		},
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccResourceVSphereConfigProfileConfigDebug(),
+			},
+			{
+				Config: testAccResourceVSphereConfigProfileConfigDebug2(),
+			},
+			{
+				Config: testAccResourceVSphereConfigProfileConfigDebug(),
+			},
+		},
+	})
+}
+
 func testAccResourceVSphereConfigProfileConfig() string {
 	return fmt.Sprintf(`
 %s
@@ -81,6 +102,94 @@ resource "vsphere_configuration_profile" "profile2" {
 `,
 		testhelper.ConfigDataRootDC1(),
 		os.Getenv("TF_VAR_VSPHERE_ESXI4"),
+		os.Getenv("TF_VAR_VSPHERE_ESXI4"),
+		os.Getenv("TF_VAR_VSPHERE_ESXI4_PASSWORD"))
+}
+
+func testAccResourceVSphereConfigProfileConfigDebug() string {
+	return fmt.Sprintf(`
+%s
+
+locals {
+  esx4_hostname = "%s"
+  esx4_password = "%s"
+}
+
+data "vsphere_host_thumbprint" "thumbprint" {
+  address  = local.esx4_hostname
+  insecure = true
+}
+
+resource "vsphere_host" "host4" {
+  hostname   = local.esx4_hostname
+  username   = "root"
+  password   = local.esx4_password
+  thumbprint = data.vsphere_host_thumbprint.thumbprint.id
+
+  datacenter = data.vsphere_datacenter.rootdc1.id
+
+  lifecycle {
+    ignore_changes = ["services", "cluster"]
+  }
+}
+
+resource "vsphere_compute_cluster" "cluster1" {
+  name                      = "cluster1"
+  datacenter_id             = data.vsphere_datacenter.rootdc1.id
+  host_system_ids           = [vsphere_host.host4.id]
+  force_evacuate_on_destroy = true
+}
+
+resource "vsphere_configuration_profile" "profile1" {
+  configuration     = file("~/Downloads/cluster1.json")
+  cluster_id        = vsphere_compute_cluster.cluster1.id
+}
+`,
+		testhelper.ConfigDataRootDC1(),
+		os.Getenv("TF_VAR_VSPHERE_ESXI4"),
+		os.Getenv("TF_VAR_VSPHERE_ESXI4_PASSWORD"))
+}
+
+func testAccResourceVSphereConfigProfileConfigDebug2() string {
+	return fmt.Sprintf(`
+%s
+
+locals {
+  esx4_hostname = "%s"
+  esx4_password = "%s"
+}
+
+data "vsphere_host_thumbprint" "thumbprint" {
+  address  = local.esx4_hostname
+  insecure = true
+}
+
+resource "vsphere_host" "host4" {
+  hostname   = local.esx4_hostname
+  username   = "root"
+  password   = local.esx4_password
+  thumbprint = data.vsphere_host_thumbprint.thumbprint.id
+
+  datacenter = data.vsphere_datacenter.rootdc1.id
+
+  lifecycle {
+    ignore_changes = ["services", "cluster"]
+  }
+}
+
+resource "vsphere_compute_cluster" "cluster1" {
+  name                      = "cluster1"
+  datacenter_id             = data.vsphere_datacenter.rootdc1.id
+  host_system_ids           = [vsphere_host.host4.id]
+  force_evacuate_on_destroy = true
+}
+
+resource "vsphere_configuration_profile" "profile1" {
+  reference_host_id = vsphere_host.host4.id
+  cluster_id        = vsphere_compute_cluster.cluster1.id
+}
+`,
+		testhelper.ConfigDataRootDC1(),
 		os.Getenv("TF_VAR_VSPHERE_ESXI4"),
 		os.Getenv("TF_VAR_VSPHERE_ESXI4_PASSWORD"))
 }

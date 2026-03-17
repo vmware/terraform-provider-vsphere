10519ca713c59f8f2bbacc442ffea5f8a755bdbe
Stoyan Zhelyazkov <stoyan.zhelyazkov@broadcom.com>
2026-03-17 15:18:20 +0200

    fix: sanitize filepaths for gosec G703
    Signed-off-by: Stoyan Zhelyazkov <stoyan.zhelyazkov@broadcom.com>

--- vsphere/config.go
+++ vsphere/config.go
@@ -284,10 +284,10 @@ func (c *Config) EnableDebug() error {
 	} else {
 		// reuse the same path
 		r = filepath.Join(r, run)
-		_ = os.RemoveAll(r) // nolint (gosec G703)
+		_ = os.RemoveAll(filepath.Clean(r))
 	}
 
-	err := os.MkdirAll(r, 0700) // nolint (gosec G703)
+	err := os.MkdirAll(filepath.Clean(r), 0700)
 	if err != nil {
 		log.Printf("[ERROR] Client debug setup failed: %v", err)
 		return err
